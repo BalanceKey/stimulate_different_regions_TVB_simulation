c1f6aae52028e16e950c4925c6e26f87c3967c4f
borana dollomaja <borana.dollomaja@univ-amu.fr>
2025-09-10 19:07:28 +0200

    Added code that pools simulations together, plots simulated data and empirical data

---
+++ src/plot_empirical_stim_data.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mne
+import os.path as op
+import sys
+sys.path.append('/Users/dollomab/MyProjects/Stimulation/VirtualEpilepsySurgery/VEP/core/')
+import vep_prepare
+roi = vep_prepare.read_vep_mrtrix_lut()
+from plot_data import plot_SEEG_timeseries
+
+subject_dir = '/Users/dollomab/MyProjects/Epinov_trial/stimulated_patients/sub-0c7ab65949e1/vep/'
+vhdr_file = subject_dir + '../ieeg/sub-0c7ab65949e1_ses-01_task-stim_run-04_ieeg.vhdr'
+# raw.info['line_freq'] = 50  # specify power line frequency to enable notch filtering
+# raw.filter(1., 150.)  # bandpass filter
+# raw.notch_filter(np.arange(50, 151, 50))  # notch filter
+# raw.resample(250.)  # resample to 250Hz
+
+# %% Read stim data
+bad_contacts = []
+remove_cerebellar = True
+raw = mne.io.read_raw_brainvision(vhdr_file, preload=True)
+raw._data *= 1e6
+fname_bad = f'{vhdr_file}.bad'                               # read the bad channel
+if op.exists(fname_bad):
+    raw.load_bad_channels(fname_bad)
+else:
+    print('>> Warning: bad channels file', fname_bad, 'not found => assuming no bad channels')
+if bad_contacts:
+    print('>> Extending bad contacts with ' + ', '.join(bad_contacts))
+    raw.info['bads'].extend(bad_contacts)
+new_names = [name.replace(" ", "") for name in raw.ch_names] # get rid of possible spaces in the names
+new_names = [name.replace(".", "") for name in new_names]
+raw.rename_channels(dict(zip(raw.ch_names, new_names)))
+seeg_xyz = vep_prepare.read_seeg_xyz(subject_dir)           # read from GARDEL file
+seeg_xyz_names = [label for label, _ in seeg_xyz]
+raw = raw.pick_channels(seeg_xyz_names)                     # check wether all GARDEL channels exist in the raw seeg file
+raw = raw.pick_types(meg=False, eeg=True, exclude="bads")
+inv_gain_file = f'{subject_dir}/elec/gain_inv-square.vep.txt'       # read gain
+invgain = np.loadtxt(inv_gain_file)
+bip_gain_inv_minus, bip_xyz, bip_name = vep_prepare.bipolarize_gain_minus(invgain, seeg_xyz, seeg_xyz_names)
+bip = vep_prepare._bipify_raw(raw)
+gain, bip = vep_prepare.gain_reorder(bip_gain_inv_minus, bip, bip_name)
+roi = vep_prepare.read_vep_mrtrix_lut()
+if remove_cerebellar:
+    cereb_cortex = ['Left-Cerebellar-cortex', 'Right-Cerebellar-cortex']
+    gain.T[roi.index('Left-Cerebellar-cortex')] = np.ones(np.shape(gain.T[-1])) * np.min(gain)
+    gain.T[roi.index('Right-Cerebellar-cortex')] = np.ones(np.shape(gain.T[-1])) * np.min(gain)
+
+#%% Plot stim data
+onset = 38.25*60
+offset = 41.05*60
+base_length = 0  # plot ts_on sec before and after
+start_idx = int((onset - base_length) * bip.info['sfreq'])
+end_idx = int((offset + base_length) * bip.info['sfreq'])
+
+y = bip.get_data()[:, start_idx:end_idx]
+t = bip.times[start_idx:end_idx]
+# ch_names = bip.ch_names 
+ch_names = [ "B1-2", "B2-3", "B4-5", "B5-6", "B6-7",
+        "B8-9", "TB1-2",  "TB4-5", "TB6-7",
+         "A1-2", "A2-3", "A3-4", "A4-5", "A5-6", "A6-7",
+         "A8-9", "A9-10", 
+         "Im1-2", "Im3-4", "Im5-6", "Im6-7",
+         "Ia1-2","Ia3-4", "Ia5-6", "Ia6-7",]
+plot_SEEG_timeseries(t, y, ch_names, bip.ch_names, scaleplt=0.0008, figsize=(20, 20)) # Plot all empirical time series
---
+++ src/plot_grouped_stim_simulations.py
@@ -0,0 +1,124 @@
+'''
+Combine all simulations together and plot the results at the SEEG level
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as signal
+import sys
+sys.path.append('/Users/dollomab/MyProjects/Stimulation/VirtualEpilepsySurgery/VEP/core/')
+import vep_prepare
+roi = vep_prepare.read_vep_mrtrix_lut()
+
+pid = 3
+patients = {1: 'sub-603cf699f88f', 2: 'sub-2ed87927ff76', 3: 'sub-0c7ab65949e1',
+            4: 'sub-9c71d0dbd98f', 5: 'sub-4b4606a742bd'}
+stim_index = 344
+
+
+data_path = f'../stim_different_roi_{patients[pid]}_stim{stim_index-2}-{stim_index}_2.npz'
+data = np.load(data_path, allow_pickle=True)
+tavg1 = data['tavg1']
+tavg2 = data['tavg2']
+tavg3 = data['tavg3']
+time1 = data['time1']
+time2 = data['time2']
+time3 = data['time3']
+
+# Group all three simulations together
+tavg = np.concatenate((tavg1[:, 0, :, 0].T, tavg2[:, 0, :, 0].T, tavg3[:, 0, :, 0].T), axis=1)
+time = np.concatenate((time1, time2 + time1[-1], time3 + time1[-1] + time2[-1]))
+
+# Plot the results
+scaleplt = 4
+plt.figure(figsize=(10, 15), tight_layout=True)
+for i in range(len(roi)):
+    plt.plot(time, tavg[i, :] * scaleplt + i + 1, 'blue', linewidth=0.5)
+plt.yticks(np.arange(len(roi)), roi)
+# plt.xlim([time[0], time[-1]])
+plt.ylim([-2, len(roi) + 1])
+plt.show()
+
+#%% Map the results to SEEG contacts
+# Load SEEG electrodes
+subject_dir = f'/Users/dollomab/MyProjects/Epinov_trial/patients/{patients[pid]}/vep'
+seeg_xyz = vep_prepare.read_seeg_xyz(subject_dir)                # read seeg electrodes information
+seeg_xyz_names = [channel_name for channel_name, _ in seeg_xyz]  # read monopolar electrode names
+#%% Load gain matrix
+gain = np.loadtxt(f'{subject_dir}/elec/gain_inv-square.vep.txt')  # import gain matrix
+assert len(seeg_xyz_names) == gain.shape[0]
+bip_gain, bip_xyz, bip_names = vep_prepare.bipolarize_gain_minus(gain, seeg_xyz, seeg_xyz_names)  # bipolarize gain
+bip_gain_prior, _, _ = vep_prepare.bipolarize_gain_minus(gain, seeg_xyz, seeg_xyz_names, is_minus=False)
+# Remove cereberall cortex influence
+bip_gain[:, roi.index('Left-Cerebellar-cortex')] = bip_gain.min()
+bip_gain[:, roi.index('Right-Cerebellar-cortex')] = bip_gain.min()
+bip_gain_prior[:, roi.index('Left-Cerebellar-cortex')] = bip_gain_prior.min()
+bip_gain_prior[:, roi.index('Right-Cerebellar-cortex')] = bip_gain_prior.min()
+
+# Normalize gain matrix
+bip_gain_norm = (bip_gain - bip_gain.min()) / (bip_gain.max() - bip_gain.min())
+bip_gain_prior_norm = (bip_gain_prior - bip_gain_prior.min()) / (bip_gain_prior.max() - bip_gain_prior.min())
+plot = False
+if plot:
+    vep_prepare.plot_gain_matrix(bip_gain, bip_names, pid)
+    plt.show()
+
+seeg_signal = np.dot(bip_gain, tavg)  # map the source space activity to seeg space
+#%% Plot the results at SEEG level
+scaleplt = 0.5
+plt.figure(figsize=(10, 15), tight_layout=True)
+for i in range(len(bip_names)):
+    plt.plot(time, (seeg_signal[i, :] - seeg_signal[i, 0]) * scaleplt + i + 1, 'blue', linewidth=0.5)
+plt.yticks(np.arange(len(bip_names)), bip_names)
+plt.xlim([time[0], time[-1]])
+plt.ylim([-2, len(bip_names) + 1])
+plt.title(f'SEEG signals - Bipolar montage - Patient {patients[pid]})')
+plt.show()
+
+# Applying a high pass filter to the signal to resseble AC recordings
+def highpass_filter(y, sr):
+    """In this case, the filter_stop_freq is that frequency below which the filter MUST act like a stop filter and filter_pass_freq is that frequency above which the filter MUST act like a pass filter.
+       The frequencies between filter_stop_freq and filter_pass_freq are the transition region or band."""
+    filter_stop_freq = 3  # Hz
+    filter_pass_freq = 3  # Hz
+    filter_order = 501
+    # High-pass filter
+    nyquist_rate = sr / 2.
+    desired = (0, 0, 1, 1)
+    bands = (0, filter_stop_freq, filter_pass_freq, nyquist_rate)
+    filter_coefs = signal.firls(filter_order, bands, desired, nyq=nyquist_rate)
+    # Apply high-pass filter
+    filtered_audio = signal.filtfilt(filter_coefs, [1], y)
+    return filtered_audio
+
+y = highpass_filter(seeg_signal, 300)  # seeg
+scaleplt = 0.5
+plt.figure(figsize=(10, 15), tight_layout=True)
+for i in range(len(bip_names)):
+    plt.plot(time, (y[i, :] - y[i, 0]) * scaleplt + i + 1, 'blue', linewidth=0.5)
+plt.yticks(np.arange(len(bip_names)), bip_names)
+plt.xlim([time[0], time[-1]])
+plt.ylim([-2, len(bip_names) + 1])
+plt.title(f'SEEG signals - Bipolar montage - Patient {patients[pid]})')
+plt.show()
+
+
+# Plot only a subset of channels
+# TODO add some noise here
+ch_names = [ "B1-2", "B2-3", "B4-5", "B5-6", "B6-7",
+        "B8-9", "TB1-2",  "TB4-5", "TB6-7",
+         "A1-2", "A2-3", "A3-4", "A4-5", "A5-6", "A6-7",
+         "A8-9", "A9-10", 
+         "Im1-2", "Im3-4", "Im5-6", "Im6-7",
+         "Ia1-2","Ia3-4", "Ia5-6", "Ia6-7",]
+scaleplt = 0.2
+plt.figure(figsize=(20, 20), tight_layout=True)
+for i in range(len(ch_names)):
+    ch_idx = bip_names.index(ch_names[i])
+    plt.plot(time, (y[ch_idx, :] - y[ch_idx, 0]) * scaleplt + i, 'blue', linewidth=1)
+plt.yticks(np.arange(len(ch_names)), ch_names, fontsize=26)
+plt.xlim([time[0], time[-1]])
+plt.ylim([-2, len(ch_names) + 1])
+plt.title(f'SEEG signals - Bipolar montage - Patient {patients[pid]})')
+plt.xlabel('Time', fontsize=50)
+plt.ylabel('Electrodes', fontsize=50)
+plt.show()
